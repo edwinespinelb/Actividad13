05a4e5a562a86e4b4ef4b2d3f30d0796d02de0fa
Edwin Espinel <e-espinel@hotmail.com>
2023-12-12 11:37:51 -0500

    ajuste de input radio

--- src/Components/encuesta.jsx
+++ src/Components/encuesta.jsx
@@ -6,121 +6,156 @@ const encuesta = () => {
         <form autoComplete="off">
           <fieldset>
             <h3>Como calificarías la elección del lugar de la fiesta?</h3>
-            <label htmlFor="excelente1">Excelente</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="excelente1"
-              id="excelente1"
-            />
-            <label htmlFor="bueno1">Bueno</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="bueno1"
-              id="bueno1"
-            />
-            <label htmlFor="regular1">Regular</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="regular1"
-              id="regular1"
-            />
-            <label htmlFor="mejora1">Necesita mejora</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="mejora1"
-              id="mejora1"
-            />
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="eleccion"
+                id="excelente1"
+              />
+              <label htmlFor="excelente1">Excelente</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="eleccion"
+                id="bueno1"
+              />
+              <label htmlFor="bueno1">Bueno</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="eleccion"
+                id="regular1"
+              />
+              <label htmlFor="regular1">Regular</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="eleccion"
+                id="mejora1"
+              />
+              <label htmlFor="mejora1">Necesita mejora</label>
+            </table>
           </fieldset>
           <fieldset>
             <h3>
               Como calificarías la atención y servicio del personal durante la
               fiesta?
             </h3>
-            <label htmlFor="excelente2">Excelente</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="excelente2"
-              id="excelente2"
-            />
-            <label htmlFor="bueno2">Bueno</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="bueno2"
-              id="bueno2"
-            />
-            <label htmlFor="regular2">Regular</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="regular2"
-              id="regular2"
-            />
-            <label htmlFor="mejora2">Necesita mejora</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="mejora2"
-              id="mejora2"
-            />
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="atencion"
+                id="excelente2"
+              />
+              <label htmlFor="excelente2">Excelente</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="atencion"
+                id="bueno2"
+              />
+              <label htmlFor="bueno2">Bueno</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="atencion"
+                id="regular2"
+              />
+              <label htmlFor="regular2">Regular</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="atencion"
+                id="mejora2"
+              />
+              <label htmlFor="mejora2">Necesita mejora</label>
+            </table>
           </fieldset>
           <fieldset>
             <h3>
               Los servicios ofrecidos durante la fiesta (Baños, Personal, etc)
               fueron satisfactorios?
             </h3>
-            <label htmlFor="sisatisfactorio">si, muy satisfactorios</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="sisatisfactorio"
-              id="sisatisfactorio"
-            />
-            <label htmlFor="aceptable">aceptable</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="aceptable"
-              id="aceptable"
-            />
-            <label htmlFor="nosatisfactorio">no, satisfactorios</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="nosatisfactorio"
-              id="nosatisfactorio"
-            />
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="servicios"
+                id="sisatisfactorio"
+              />
+              <label htmlFor="sisatisfactorio">si, muy satisfactorios</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="servicios"
+                id="aceptable"
+              />
+              <label htmlFor="aceptable">aceptable</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="servicios"
+                id="nosatisfactorio"
+              />
+              <label htmlFor="nosatisfactorio">no, satisfactorios</label>
+            </table>
           </fieldset>
           <fieldset>
             <h3>La duración de la fiesta fue adecuada para ti?</h3>
-            <label htmlFor="siperfecta">si, perfecta</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="siperfecta"
-              id="siperfecta"
-            />
-            <label htmlFor="unpococorta">un poco corta</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="unpococorta"
-              id="unpococorta"
-            />
-            <label htmlFor="unpocolarga">un poco larga</label>
-            <input
-              className="check"
-              type="checkbox"
-              name="unpocolarga"
-              id="unpocolarga"
-            />
-            <label htmlFor="larga">Demasiada larga</label>
-            <input className="check" type="checkbox" name="larga" id="larga" />
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="duracion"
+                id="siperfecta"
+              />
+              <label htmlFor="siperfecta">si, perfecta</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="duracion"
+                id="unpococorta"
+              />
+              <label htmlFor="unpococorta">un poco corta</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="duracion"
+                id="unpocolarga"
+              />
+              <label htmlFor="unpocolarga">un poco larga</label>
+            </table>
+            <table>
+              <input
+                className="check"
+                type="radio"
+                name="duracion"
+                id="larga"
+              />
+              <label htmlFor="larga">Demasiada larga</label>
+            </table>
           </fieldset>
         </form>
       </div>
